435a3b412de6aae44b5091d437baab2cabb604c6
Matt Weagle <mweagle@adobe.com>
2015-06-13 10:05:54 -0700

    Disable single unit test helper

--- src/test/java/tereus/SingleEvaluationTest.java
+++ src/test/java/tereus/SingleEvaluationTest.java
@@ -14,8 +14,10 @@ public class SingleEvaluationTest extends EvaluationTest {
 
     @Test
     public void test() throws Exception {
-    	final Path evaluationFilepath = Paths.get("/Users/mweagle/Documents/GitHub/Tereus/src/test/java/tereus/evaluation/embedding/definition/file.js");
-    	final Path expectedFilepath = Paths.get("/Users/mweagle/Documents/GitHub/Tereus/src/test/java/tereus/evaluation/embedding/expected/file.json");
+    	/*
+    	final Path evaluationFilepath = Paths.get("/Users/mweagle/Documents/GitHub/Tereus/src/test/java/tereus/evaluation/aws_samples/definition/EC2InstanceWithSecurityGroupSample.js");
+    	final Path expectedFilepath = Paths.get("/Users/mweagle/Documents/GitHub/Tereus/src/test/java/tereus/evaluation/aws_samples/expected/EC2InstanceWithSecurityGroupSample.json");
         super.verifyEvaluation(evaluationFilepath, expectedFilepath);
+        */
     }
 }
