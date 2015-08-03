a3381b2d257638317795e5d5bb24bc36be36eea0
Matt Weagle <mweagle@gmail.com>
2015-08-03 14:38:00 -0700

    Cleanup test file

--- patch.js
+++ patch.js
@@ -1,10 +1,7 @@
+/* global JSONPatch,Patch */
 JSONPatch("SomePatch")({
   "Resources":
   {
     "MyEc2" : Patch.Add("Foobar")
   }
-})
-
-
-doc = JSON8Patch.add({}, '/foo', 'foo')[0]
-logger.info('TRANSFORMED: ' + JSON.stringify(doc, null,  ' '));
+});
