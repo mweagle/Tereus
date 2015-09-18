47947452249b34380213561ce52b16eee6ce86b2
Matt Weagle <mweagle@gmail.com>
2015-09-18 07:58:51 -0700

    Remove StackName target

--- src/test/java/tereus/update/core/definition/simple.js
+++ src/test/java/tereus/update/core/definition/simple.js
@@ -1,5 +1,9 @@
 /* global CloudFormationUpdate,Patch */
-CloudFormationUpdate("SomePatch")({
+/**
+ * Don't specify a stack name target in the CloudFormationUpdate, since
+ * we don't have a predefined stack to target.
+ */
+CloudFormationUpdate()({
   "Key":
   {
     "Subkey" : Patch.Add("Value")
