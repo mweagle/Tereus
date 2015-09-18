059f0975c51db2fa9486acd25e5ec64bd16671df
Matt Weagle <mweagle@gmail.com>
2015-09-18 08:24:47 -0700

    Prefer lazy initialization over Class.forName shenanigans

--- src/main/java/com/mweagle/tereus/NashornEvaluator.java
+++ src/main/java/com/mweagle/tereus/NashornEvaluator.java
@@ -49,149 +49,114 @@ import com.google.common.base.Charsets;
 
 public abstract class NashornEvaluator implements INashornEvaluatorContext
 {
-	protected void publishGlobals(ScriptEngine engine)
-	{
-		// NOP
-	}
+    protected void publishGlobals(ScriptEngine engine)
+    {
+        // NOP
+    }
 
-	protected Stream<String> javaClassnames()
-	{
-		return Stream.empty();
-	}
+    protected Stream<INashornEvaluationAccumulator> evaluationAccumulators()
+    {
+        return Stream.empty();
+    }
 
-	protected Stream<String> javascriptResources()
-	{
-		return Stream.empty();
-	}
+    protected Stream<String> javascriptResources()
+    {
+        return Stream.empty();
+    }
 
-	public HashMap<String, Object> run(final Path jsFilePath, final Logger logger) throws Exception
-	{
-		final Instant startTime = Instant.now();
-		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    public HashMap<String, Object> run(final Path jsFilePath, final Logger logger) throws Exception
+    {
+        final Instant startTime = Instant.now();
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 
-		// Globals
-		this.publishGlobals(engine);
+        // Globals
+        this.publishGlobals(engine);
 
-		// Get all the JavaClasses that should be registered and map them in
-		// there...
-		List<INashornEvaluationAccumulator> boundInstances = this.javaClassnames().map(new EngineBinding(this, engine))
-				.collect(Collectors.toList());
+        // Get all the JavaClasses that should be registered and map them in
+        // there...
+        List<INashornEvaluationAccumulator> boundInstances = evaluationAccumulators().map(eachObject ->
+        {
+            final String boundName = eachObject.bind(this);
+            engine.put(boundName, eachObject);
+            return eachObject;
+        }).collect(Collectors.toList());
 
-		// Add the custom JavaScript classes to the evaluation context
-		this.javascriptResources().forEach(new JavaScriptInputStreamPublisher(engine, logger));
-		
-		// Evaluate the template
-		try
-		{
-	        final String jsFile = new String(Files.readAllBytes(jsFilePath), Charsets.UTF_8);
-			engine.eval(jsFile);
-		} catch (Exception ex)
-		{
-			final String msg = String.format("Failed to evaluate: %s%n%s: %s", jsFilePath, ex.getClass().getName(),
-					ex.getMessage());
-			throw new Exception(msg, ex.getCause());
-		}
+        // Add the custom JavaScript classes to the evaluation context
+        this.javascriptResources().forEach(new JavaScriptInputStreamPublisher(engine, logger));
 
-		HashMap<String, Object> result = new HashMap<>();
-		for (INashornEvaluationAccumulator eachBinding : boundInstances)
-		{
-			if (null != eachBinding.getAccumulationResult())
-			{
-				result.putAll(eachBinding.getAccumulationResult());
-			}
-		}
-		final Duration totalTime = Duration.between(startTime, Instant.now());
-		logger.info("Template evaluation duration: {} ms", totalTime.toMillis());
-		return result;
-	}
+        // Evaluate the template
+        try
+        {
+            final String jsFile = new String(Files.readAllBytes(jsFilePath), Charsets.UTF_8);
+            engine.eval(jsFile);
+        } catch (Exception ex)
+        {
+            final String msg = String.format("Failed to evaluate: %s%n%s: %s", jsFilePath, ex.getClass().getName(),
+                    ex.getMessage());
+            throw new Exception(msg, ex.getCause());
+        }
 
-	protected abstract class JavaScriptPublisher implements Consumer<String>
-	{
-		final ScriptEngine engine;
-		final Logger logger;
+        HashMap<String, Object> result = new HashMap<>();
+        for (INashornEvaluationAccumulator eachBinding : boundInstances)
+        {
+            if (null != eachBinding.getAccumulationResult())
+            {
+                result.putAll(eachBinding.getAccumulationResult());
+            }
+        }
+        final Duration totalTime = Duration.between(startTime, Instant.now());
+        logger.info("Template evaluation duration: {} ms", totalTime.toMillis());
+        return result;
+    }
 
-		public JavaScriptPublisher(ScriptEngine engine, Logger logger) {
-			this.engine = engine;
-			this.logger = logger;
-		}
+    protected abstract class JavaScriptPublisher implements Consumer<String>
+    {
+        final ScriptEngine engine;
+        final Logger logger;
 
-		void evaluate(Reader jsReader, String errorMessage)
-		{
-			boolean evaluated = false;
-			try
-			{
-				this.engine.eval(jsReader);
-				evaluated = true;
-			} catch (Exception ex)
-			{
-				this.logger.error(ex);
-				throw new RuntimeException(ex);
-			} finally
-			{
-				if (!evaluated)
-				{
-					this.logger.error(errorMessage);
-				}
-			}
-		}
-	}
+        public JavaScriptPublisher(ScriptEngine engine, Logger logger) {
+            this.engine = engine;
+            this.logger = logger;
+        }
 
-	protected class JavaScriptInputStreamPublisher extends NashornEvaluator.JavaScriptPublisher
-	{
-		public JavaScriptInputStreamPublisher(ScriptEngine engine, final Logger logger) {
-			super(engine, logger);
-		};
+        void evaluate(Reader jsReader, String errorMessage)
+        {
+            boolean evaluated = false;
+            try
+            {
+                this.engine.eval(jsReader);
+                evaluated = true;
+            } catch (Exception ex)
+            {
+                this.logger.error(ex);
+                throw new RuntimeException(ex);
+            } finally
+            {
+                if (!evaluated)
+                {
+                    this.logger.error(errorMessage);
+                }
+            }
+        }
+    }
 
-		@Override
-		public void accept(final String resourcePath)
-		{
-			this.logger.debug("Loading resource: " + resourcePath);
-			final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
-			if (null == in)
-			{
-				throw new RuntimeException("Failed to load JS resource: " + resourcePath);
-			}
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
-			super.evaluate(reader, "Failed to load resource: " + resourcePath);
-		}
-	}
+    protected class JavaScriptInputStreamPublisher extends NashornEvaluator.JavaScriptPublisher
+    {
+        public JavaScriptInputStreamPublisher(ScriptEngine engine, final Logger logger) {
+            super(engine, logger);
+        };
 
-	protected static class EngineBinding implements Function<String, INashornEvaluationAccumulator>
-	{
-		private final INashornEvaluatorContext evaluationContext;
-		private final ScriptEngine engine;
-
-		public EngineBinding(INashornEvaluatorContext context, ScriptEngine engine) {
-			this.evaluationContext = context;
-			this.engine = engine;
-		}
-
-		@Override
-		public INashornEvaluationAccumulator apply(String className)
-		{
-			try
-			{
-				Class<?> clazz = Class.forName(className);
-				Constructor<?> ctor = null;
-				INashornEvaluationAccumulator bound = null;
-				try
-				{
-					ctor = clazz.getConstructor(INashornEvaluatorContext.class);
-					bound = (INashornEvaluationAccumulator) ctor.newInstance(this.evaluationContext);
-				} catch (NoSuchMethodException ex)
-				{
-					ctor = clazz.getConstructor();
-					bound = (INashornEvaluationAccumulator) ctor.newInstance();
-				}
-				engine.put(bound.getAccumulatorName(), bound);
-				return bound;
-			} catch (RuntimeException e)
-			{
-				throw e;
-			} catch (Exception e)
-			{
-				throw new RuntimeException(e);
-			}
-		}
-	}
+        @Override
+        public void accept(final String resourcePath)
+        {
+            this.logger.debug("Loading resource: " + resourcePath);
+            final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath);
+            if (null == in)
+            {
+                throw new RuntimeException("Failed to load JS resource: " + resourcePath);
+            }
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
+            super.evaluate(reader, "Failed to load resource: " + resourcePath);
+        }
+    }
 }
