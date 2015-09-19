b6b79d045035e8537bd2f5f3340b959653ed57a1
Matt Weagle <mweagle@gmail.com>
2015-09-19 09:25:07 -0700

    Eliminate eval

--- examples/lambda/run.sh
+++ examples/lambda/run.sh
@@ -2,5 +2,5 @@
 TEREUS_JAR_ARG=${TEREUS_JAR:-"../../build/libs/Tereus.jar"}
 S3_BUCKET_ARG=${S3_BUCKET:-"weagle"}
 AWS_REGION_ARG=${AWS_DEFAULT_REGION:-"us-west-2"}
-eval "java -jar $TEREUS_JAR_ARG create --template stack.js --bucket $S3_BUCKET_ARG  --region $AWS_REGION_ARG --dry-run"
-eval "java -jar $TEREUS_JAR_ARG update --patch patch.js --region $AWS_REGION_ARG  --dry-run"
+java -jar $TEREUS_JAR_ARG create --template stack.js --bucket $S3_BUCKET_ARG  --region $AWS_REGION_ARG --dry-run
+java -jar $TEREUS_JAR_ARG update --patch patch.js --region $AWS_REGION_ARG  --dry-run
